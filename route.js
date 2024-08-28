cb4eb83ce6c994e2bfd29268360a496b2beb412e
ankurana17 <119804856+ankurana17@users.noreply.github.com>
2024-08-28 17:33:18 +0530

    signup page added

--- route.js
+++ route.js
@@ -7,5 +7,43 @@ router.get("/",(req,res)=>
     res.render("index")
     res.end()
 })
-
+router.use("/signup",(req,res)=>{
+    if(req.method==='GET'){
+        res.render("signup")
+        res.end()
+    }
+    else
+    {
+    mysql_connect.getConnection((err,connection)=>
+        {
+        if(err){
+            connection.release()
+            res.send(err)
+            res.end()
+        }
+        else{
+            var firstname=req.body.firstname
+            var lastname=req.body.lastname
+            var email=req.body.email
+            var password=req.body.password
+            var confirmpassword=req.body.confirmpassword
+            var phoneno=req.body.phoneno
+            var gender=req.body.gender
+            const q=`insert into signup(firstname,lastname,email,password,phoneno,gender,confirmpassword)values('${firstname}','${lastname}','${email}','${password}','${confirmpassword}''${phoneno}','${gender}',)`
+            connection.query(q,(err)=>
+            {
+                if(err){
+                    connection.release()
+                    res.render()
+                    res.end()
+                }
+                else{
+                    res.render('index',{message:email+"added successfully"})
+                    res.end()
+                }
+            })
+        }
+    })
+    }
+})
 module.exports=router
